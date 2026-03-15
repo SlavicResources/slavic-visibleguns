5c0f880ef133342e49e632b0bcedd764a062a537
Slavic Resources <slavicslides@gmail.com>
2026-03-15 22:01:40 +0800

    config.lua

---
+++ config.lua
@@ -0,0 +1,91 @@
+Config = {}
+
+Config.WeaponTypes = {
+---Handguns
+["weapon_pistol"] = "handgun",
+["weapon_pistol_mk2"] = "handgun",
+["weapon_combatpistol"] = "handgun",
+["weapon_appistol"] = "handgun",
+["weapon_stungun"] = "handgun",
+["weapon_flaregun"] = "handgun",
+["weapon_marksmanpistol"] = "handgun",
+["weapon_revolver"] = "handgun",
+["weapon_revolver_mk2"] = "handgun",
+["weapon_doubleaction"] = "handgun",
+["weapon_raypistol"] = "handgun",
+["weapon_microsmg"] = "handgun",
+["weapon_minismg"] = "handgun",      
+["weapon_machinepistol"] = "handgun",   
+
+---Rifles (all other weapons)
+["weapon_smg"] = "rifle",
+["weapon_smg_mk2"] = "rifle",
+["weapon_assaultsmg"] = "rifle",
+["weapon_combatpdw"] = "rifle",
+["weapon_pumpshotgun"] = "rifle",
+["weapon_pumpshotgun_mk2"] = "rifle",
+["weapon_sawnoffshotgun"] = "rifle",
+["weapon_assaultshotgun"] = "rifle",
+["weapon_bullpupshotgun"] = "rifle",
+["weapon_musket"] = "rifle",
+["weapon_carbinerifle"] = "rifle",
+["weapon_carbinerifle_mk2"] = "rifle",
+["weapon_assaultrifle"] = "rifle",
+["weapon_assaultrifle_mk2"] = "rifle",
+["weapon_advancedrifle"] = "rifle",
+["weapon_specialcarbine"] = "rifle",
+["weapon_specialcarbine_mk2"] = "rifle",
+["weapon_bullpuprifle"] = "rifle",
+["weapon_bullpuprifle_mk2"] = "rifle",
+["weapon_marksmanrifle"] = "rifle",
+["weapon_precisionrifle"] = "rifle",
+["weapon_mg"] = "rifle",
+["weapon_combatmg"] = "rifle",
+["weapon_combatmg_mk2"] = "rifle",
+["weapon_rpg"] = "rifle",
+["weapon_grenadelauncher"] = "rifle",
+["weapon_minigun"] = "rifle",
+["weapon_knife"] = "rifle",
+["weapon_nightstick"] = "rifle",
+["weapon_hammer"] = "rifle",
+["weapon_bat"] = "rifle",
+["weapon_crowbar"] = "rifle",
+["weapon_wrench"] = "rifle",
+["weapon_grenade"] = "rifle",
+["weapon_stickybomb"] = "rifle",
+["weapon_molotov"] = "rifle",
+["weapon_smokegrenade"] = "rifle",
+["weapon_proxmine"] = "rifle",
+["weapon_bzgas"] = "rifle",
+["weapon_pipebomb"] = "rifle",
+["weapon_petrolcan"] = "rifle",
+["weapon_fireextinguisher"] = "rifle",
+["weapon_ball"] = "rifle",
+["weapon_flare"] = "rifle",
+["weapon_parachute"] = "rifle",
+["weapon_firework"] = "rifle"
+}
+
+Config.Positions = {
+
+    handgun = {
+        bone = 51826, -- thigh
+        x = 0.10,
+        y = 0.0,
+        z = 0.11,
+        xr = -90.0,
+        yr = 0.0,
+        zr = 0.0
+    },
+
+    rifle = {
+        bone = 24816, -- spine
+        x = 0.15,
+        y = -0.20,
+        z = 0.02,
+        xr = 0.0,
+        yr = 150.0,
+        zr = 0.0
+    }
+
+}
